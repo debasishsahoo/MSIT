01dec3800f4704a9380e142d6a1164849586ac3b
debasishsahoo <debasish.sahoo96@gmail.com>
2022-08-03 12:34:00 +0530

    day 2.2

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dbconnect = require('./db')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config();
 
@@ -10,9 +10,28 @@ const host = process.env.HOST;
 const app = express();
 
 
+async function connect() {
+    const dbuser = process.env.DBUSER;
+    const dbpass = process.env.DBPASS;
+    const dbname = process.env.DBNAME;
+
+    const url = `mongodb+srv://${dbuser}:${dbpass}@cluster0.lttvi.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+    try {
+        await mongoose.connect(url);
+        console.log('DB Connection Done.......')
+    }
+    catch (err) {
+        console.log(err)
+        console.log(`Error in DB Connection,` + JSON.stringify(err, undefined, 2));
+        process.exit();
+    }
+}
+
+
 app.listen(port, host, async () => {
     //first DB Connection
-    await dbconnect;
+    await connect();
     //Second App connection
     console.log(`The App lisning at http://${host}:${port}`)
 });
