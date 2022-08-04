69a27fd5d92e5c1b09b43a5b9d934e1a3cf8cfb6
debasishsahoo <debasish.sahoo96@gmail.com>
2022-08-04 12:37:49 +0530

    Day 3.6

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,7 +1,31 @@
 const UserModel = require('../model/user.model');
 
 const UserController = {
-  signup: async (req, res, next) => { res.send('Working') },
+  signup: async (req, res, next) => {
+    const { name, email, mobile, password } = req.body;
+
+    try {
+      const sendData = await UserModel.create({
+        name, email, mobile, password
+      })
+      res.status(201).json({
+        msg: "User is Created! Congrats",
+        user: sendData
+      });
+    }
+    catch (error) {
+      res.status(404).json({
+        msg: error.message,
+      })
+    }
+
+
+
+
+
+
+
+  },
   signin: async (req, res, next) => { res.send('Working') },
   profileEdit: async (req, res, next) => { res.send('Working') },
   profileDelete: async (req, res, next) => { res.send('Working') },
